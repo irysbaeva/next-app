9ef40b278abad0047b7a796f03b851b51cde6065
Irina Rysbaeva <irsbv@mail.ru>
2021-10-20 13:56:11 +0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MainContainer from "../components/MainContainer";
+
+const Index = () => {
+  return (
+    <MainContainer keywords={"main page"}>
+      <h1>Main page</h1>
+    </MainContainer>
+  );
+};
+
+export default Index;
